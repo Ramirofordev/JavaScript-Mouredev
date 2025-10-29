fc9c028b4ebee83e54a06e33fd6ec04e4c65b6ad
Kuro <joseignacioramirocastro22@gmail.com>
2025-10-29 12:11:25 +0100

    Finalizado el apartado de 'Hola mundo'

---
+++ Basic/00-helloworld.js
@@ -0,0 +1,29 @@
+// Esto es un comentario simple en JavaScript
+
+/* 
+Esto es 
+un comentario
+de multiples lineas
+*/
+
+
+console.log("Hola, JavaScript!")
+console.log('Hola, JavaScript!')
+console.log('Hola, JavaScript!')
+
+// console.log("Hola, JavaScript!")
+
+/*
+console.log("Hola, JavaScript!")
+console.log('Hola, JavaScript!')
+console.log('Hola, JavaScript!')
+*/
+
+console.log(5)
+console.log("5")
+console.log(5 + 2)
+console.log(5 - 2)
+console.log(5 * 2)
+console.log(5 / 2)
+console.log(5 % 2)
+console.log(5 ** 2)
--- Readme.md
+++ Readme.md
@@ -1,1 +1,4 @@
 # Este es un repositorio del curso de JavaScript de Mouredev
+
+El fin de este repositorio es documentar todo el **aprendizaje obtenido** en mi cuenta de GitHub y practicar lo aprendido en el curso de Git.
+
